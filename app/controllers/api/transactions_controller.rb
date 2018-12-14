79166d9372fed224b697548eac94d9052f39b311
nagel <kevin.jerome.nagel@gmail.com>
2018-12-13 21:35:36 -0600

    Added functionality for automatic custom category update based on historical transaction and category history

--- app/controllers/api/transactions_controller.rb
+++ app/controllers/api/transactions_controller.rb
@@ -6,107 +6,67 @@ class Api::TransactionsController < ApplicationController
 
     render "index.json.jbuilder"
 
-    #CATEGORY VIEW --> Assign users all category IDs and then let the user make their own custom categories. The custom categories will cover mutliple Plaid provided categories. When a user assigns a custom category to a transaction, all future transactions with the Plaid category will be assigned to that custom category
-
-    # client = Plaid::Client.new(
-    #   env: :development,
-    #   client_id: '5bd7725bca639100112a4e11',
-    #   secret: '7c256eddd5b2b3cc68e49070348ae3',
-    #   public_key: '7a7b2dccb03c34134f5e2b97c28d5d'
-    # )
-
-    # # Pulls all category info from Plaid
-    # all_categories = client.categories.get()
-    # api_categories = all_categories.categories
-
-    # # Saves Plaid categories to database for user to assign custom categories to
-    # api_categories.each do |api_category|
-
-    #   @plaid_category = PlaidCategory.new({
-    #     plaid_category_API_ID: api_category.category_id,
-    #     plaid_category_name: api_category.hierarchy.join("-"),
-    #   })
-    #   @plaid_category.save
-
-    # end
-
-
-
   end
 
-  # Currently handled by item create
-  def create #call from API to populate
+  # For new transactions that are not being updated from the Plaid API
+    # Assigns custom category to transaction if similar transaction already occurred 
+  def create
 
     client = @@client
 
-    # Pull transactions for a date range
-    # response = client.transactions.get('access-development-b5e0a275-a8a9-45bb-877d-662ede64c192',
-    #                                     '2018-01-01',
-    #                                     '2018-02-01')
+    # Retrieves transaction response from Plaid for specific item
+    access_token = Item.find_by(id: params[:id]).access_token
 
-    # Manipulate the count and offset parameters to paginate
-    # transactions and retrieve all available data
-    response = client.transactions.get("access-development-b5e0a275-a8a9-45bb-877d-662ede64c192",
-                                        Date.today.at_beginning_of_month,
-                                        Date.today.end_of_month)
-                                        # count: 250,
-                                        # offset: 0)
+    puts "Access Token: " + access_token
 
-    # Pulls only individual transaction info
+    # "access-sandbox-9e993d3f-b330-4ed2-b0bf-c7cbdad9c85d" 
+    response = client.transactions.get(access_token, Date.today.at_beginning_of_month, Date.today.end_of_month)
     transactions = response.transactions
 
-    # pp transactions
-
+    # Loop through transactions response. Assign custom category to new transaction if the plaid_category_API_ID already has a custom category assigned. 
     transactions.each do |transaction|
 
-      puts "Transaction ID: " + transaction.transaction_id
-      puts "Account ID: " + transaction.account_id
-      puts "Category ID: " + transaction.category_id
-      puts "User ID: " + "use current_user.id here"
-      puts "Name: " + transaction.name
-      puts "Amount: " + transaction.amount.to_s
-      puts "Date: " + transaction.date
-      puts "Type: " + transaction.transaction_type
-      puts "Currency Type: " + transaction.iso_currency_code
-      puts "Pending: " + transaction.pending.to_s
+      # Queries database for any existing transaction with the same plaid_category_API_ID
+      # matchings_trans_category = Transaction.find_by( ['user_id = :user_id and "plaid_category_API_ID" = :category', {user_id: current_user.id, category: transaction.category_id}])
+
+      matchings_trans_category = Transaction.where(user_id: current_user.id).where(plaid_category_API_ID: transaction.category_id).where.not(category_id: nil)
+
       puts ""
+      puts "-------------------------------------"
+      pp matchings_trans_category
+      puts "-------------------------------------"
       puts ""
+
+      # Finds account ID from Account Model based on Plaid Account ID
+      account = Account.find_by(account_id_plaid: transaction.account_id)
+      account_id = account.id
 
       # Converts string to decimal 
       amount = transaction.amount.to_s
       amount_decimal = amount.to_d
 
       @transaction = Transaction.new({
-        transaction_id: transaction.transaction_id,
-        account_id_plaid: transaction.account_id,
-        account_id: 1, #use Active record query based on the plaid account ID
-        category_id: 1, #transaction.category_id, #use Active record query based on the plaid account ID
-        user_id: 1, #replace with current_user.id 
         name: transaction.name,
+        plaid_category_API_ID: transaction.category_id,
+        account_id_plaid: transaction.account_id,
+        user_id: current_user.id,
+        transaction_id: transaction.transaction_id, 
         amount: amount_decimal,
+        account_id: account_id,
         transaction_date: transaction.date,
         transaction_type: transaction.transaction_type,
         currency_type: transaction.iso_currency_code,
         pending: transaction.pending
       })
+      if matchings_trans_category.present?
+        @transaction.category_id = matchings_trans_category.first.category_id
+      end
       @transaction.save!
 
-    end
-
-    # {"account_id"=>"9BpjXdokJYTVw7Vza07JIykONOJvBXhdZPJxn",
-    # "amount"=>718.2,
-    # "category"=>["Transfer", "Third Party", "Chase QuickPay"],
-    # "category_id"=>"21010007",
-    # "date"=>"2018-01-31",
-    # "iso_currency_code"=>"USD",
-    # "location"=>{},
-    # "name"=>"QuickPay with Zelle payment to Chris Sanchez 6865180886",
-    # "payment_meta"=>{},
-    # "pending"=>false,
-    # "transaction_id"=>"RNz1XmxpbaSagNav8zNkigDYZav9NVuyjVyAb",
-    # "transaction_type"=>"special"}
+    end 
 
     render "show.json.jbuilder"
+
   end
 
   def show
@@ -131,9 +91,7 @@ class Api::TransactionsController < ApplicationController
     @transaction.category_id = params[:category_id] || @transaction.category_id
 
     @transaction.save
-
-    render "show.json.jbuilder"
-  end
+  end 
 
   def destroy
     @item = Item.find_by(id: params[:id])
@@ -141,5 +99,4 @@ class Api::TransactionsController < ApplicationController
 
     render json: {message: "Transaction deleted!"}
   end
-
-end
+end 
